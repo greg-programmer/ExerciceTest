939a680b7435c779ed4b4464b09036846b225388
Grégory <gregory.schoemaecker@outlook.fr>
2023-08-27 08:03:49 +0200

    Le test 2 est terminé

--- Exercice04biblio/City.cs
+++ Exercice04biblio/City.cs
@@ -1,17 +1,31 @@
 ﻿using Exercice04bilioTest.Tests;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Exercice04biblio
 {
     public class City
     {
-        public string CityName { get; set; }
+        private List<string> CitysList { get; set; } = new List<string> 
+        { 
+            "Paris","Budapest","Skopje"," Rotterdam",
+            "Valence","Vancouver","Amsterdam","Vienne",
+            "Sydney","New York","Londres","Bangkok",
+            "Hong Kong", "Dubaï","Rome","Istanbul", 
+            "Dunkerque"
+        }; 
       
 
-        public string CitySearch(string searchText)
+        public List<string> CitySearch(string searchText)
         {
             if(searchText.Length < 2)
             {
                 throw new NotFoundException("erreur");
+            }
+            if(searchText.Length >= 2)
+            {
+
+                var a = CitysList.Where(a => a.Contains(searchText)).ToList();
+                return a;
             }
               throw new NotImplementedException();                    
             
--- Exercice04bilioTest.Tests/UnitTest1.cs
+++ Exercice04bilioTest.Tests/UnitTest1.cs
@@ -12,9 +12,19 @@ namespace Exercice04bilioTest.Tests
         //Si le texte de la recherche contient moins de 2 caractères, Une exception est levée de type NotFoundException.
         public void When_TextSearch_Contains_LessThan2Chars_Then_NotFoundException()
         {
-            City city = new City();           
-            string ArgCity ;                
+            City city = new City();                       
             Assert.ThrowsException<NotFoundException>(() => city.CitySearch("h") );
+        }
+        // autre version si le cas de test était "est une liste qui ne contient que 0"   
+        //Si le texte de recherche est égal ou supérieur à 2 caractères,
+        //il doit renvoyer tous les noms de ville commençant par le texte de recherche exact.
+        //Par exemple, pour le texte de recherche "Va", la fonction doit renvoyer Valence et Vancouver
+        [TestMethod]
+        public void When_TextSearch_Equal_Or_MoreThan2Chars_Then_CitysList()
+        {
+            List<string> list = new List<string> {"Valence", "Vancouver" };
+            City city = new City();
+            CollectionAssert.AreEqual(list,city.CitySearch("Va"));
         }
     }
 }
